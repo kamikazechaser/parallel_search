e64a7ee9ebe874aa25f32ef97464881be38b6ae5
Mohammed Sohail <sohailsameja@gmail.com>
2019-11-30 12:03:22 +0530

    code: format

--- linsearchpar.c
+++ linsearchpar.c
@@ -2,68 +2,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/time.h>
+
 #define int long long int
-// Load all number from "path" to "array"
-void loadNumbers(char *path, int *array, int size) {
-  // double diff;
-  // double start = omp_get_wtime();
-  FILE *fp;
-  fp = fopen(path, "r");
-  int i = 0;
-  while (i < size) {
-    fscanf(fp, "%d", &array[i]);
-    i++;
-  }
-  // double end = omp_get_wtime();
-  // diff = end - start;
-  // printf("Time for file read = %lf\n", diff);
-}
 
-// Use Linear Search to look for number "lookingFor" in array "numbers"
-void linearSearch(int *numbers, int size, int lookingFor) {
-  double diff;
-  int tid;
-
-  // Linear Search
-  int i;
-  int foundNumber = 0;
-  double start = omp_get_wtime();
-#pragma omp parallel num_threads(2) private(tid) shared(foundNumber)
-  {
-    tid = omp_get_thread_num();
-    // printf("%d", tid);
-#pragma omp for
-    for (i = 0; i < size; i++) {
-
-      if (lookingFor == numbers[i]) {
-        // if found the number, stop all the threads
-        foundNumber = 1;
-        // printf("T%d :: Found Number :: I = %d\n", tid, i);
-      }
-      if (foundNumber) {
-        i = size;
-      }
+// read dataset into an array
+void loadNumbers(char * path, int * array, int size) {
+    FILE * fp;
+    fp = fopen(path, "r");
+    int i = 0;
+    while (i < size) {
+        fscanf(fp, "%d", & array[i]);
+        i++;
     }
-  }
-  double end = omp_get_wtime();
-  diff = end - start;
-  printf("Time: %.1lf", diff * 1000000L);
 }
 
-int main(int argc, char *argv[]) {
-  // if (argc < 3) {
-  //   printf("Usage: ./par sizeOfInput filepath");
-  // }
-  // printf("%d", atoi(argv[1]));
-  int SIZE = atoi(argv[1]);
-  // Test cases:
-  // Number 1 is at the end of the array (length - 1)
-  // Number 2 is at the beginning (index 7)
-  int FIND_NUMBER = 1;
-  int *numbers = (int *)malloc(SIZE * sizeof(int));
-  loadNumbers(argv[2], numbers, SIZE);
-  linearSearch(numbers, SIZE, FIND_NUMBER);
-  free(numbers);
-  return 0;
+// linear search function
+void linearSearch(int * numbers, int size, int lookingFor) {
+    double diff;
+    int tid, i;
+    
+    int foundNumber = 0;
+    double start = omp_get_wtime();
+    
+    // do in parallel
+    #pragma omp parallel num_threads(2) private(tid) shared(foundNumber)
+    {
+        tid = omp_get_thread_num();
+        #pragma omp for
+        for (i = 0; i < size; i++) {
+            if (lookingFor == numbers[i]) {
+                foundNumber = 1;
+                printf("found at position: %d on processor: %d\n", i, tid);
+            }
+            if (foundNumber) {
+                i = size;
+            }
+        }
+    }
+    
+    double end = omp_get_wtime();
+    diff = end - start;
+    
+    printf("time: %.1lf\n", diff*1000000L);
+}
+
+int main(int argc, char * argv[]) {
+    int SIZE = atoi(argv[1]);
+    int FIND_NUMBER = 1;
+    
+    int * numbers = (int*) malloc(SIZE * sizeof(int));
+    
+    loadNumbers(argv[2], numbers, SIZE);
+    linearSearch(numbers, SIZE, FIND_NUMBER);
+    free(numbers);
+    
+    return 0;
 }
